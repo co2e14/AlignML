f1094a13b738f2ff13c746c4b9966c5d9ec68781
co2e14 <christian.orr@diamond.ac.uk>
2024-06-03 14:02:44 +0100

    collect data script fully functional

--- collectData.py
+++ collectData.py
@@ -1,24 +1,46 @@
 import numpy as np
 from cothread.catools import *
 from datetime import datetime
+import sys
 
 now = datetime.now()
 
-def getReadbacks():
-    x = caget('BL23I-MO-GONIO-01:X')
-    y = caget('BL23I-MO-GONIO-01:Y')
-    z = caget('BL23I-MO-GONIO-01:Z')
+def getZero():
+    x = np.round(float(caget('BL23I-MO-GONIO-01:X')), 4)
+    y = np.round(float(caget('BL23I-MO-GONIO-01:Y')), 4)
+    z = np.round(float(caget('BL23I-MO-GONIO-01:Z')), 4)
+
+    return x, y, z
+
+def getReadbacks(xZ, yZ, zZ):
+    x = np.round(float(caget('BL23I-MO-GONIO-01:X')), 4) - xZ
+    y = np.round(float(caget('BL23I-MO-GONIO-01:Y')), 4) - yZ
+    z = np.round(float(caget('BL23I-MO-GONIO-01:Z')), 4) - zZ
     #omega = caget('BL23I-MO-GONIO-01:OMEGA')
-    kappa = caget('BL23I-MO-GONIO-01:KAPPA')
-    phi = caget('BL23I-MO-GONIO-01:PHI')
-    
-    vals = [np.round(float(x), 4), np.round(float(y), 4), np.round(float(z), 4), np.round(float(kappa), 1), np.round(float(phi), 1)]
+    kappa = np.round(caget('BL23I-MO-GONIO-01:KAPPA'), 1)
+    phi = np.round(caget('BL23I-MO-GONIO-01:PHI'), 1)
+    if phi == -0.0:
+        phi = 0.0
+
+    vals = [x, y, z, kappa, phi]
 
     return vals
 
 if __name__ == "__main__":
-    RBVS = getReadbacks()
-    print(RBVS)
-    with open(f'rbvs_{now.strftime("%d%m%Y")}.txt', 'a') as f:
-        f.write(str(RBVS))
-        f.write('\n')
+    start = input("Is the pin centered at kappa/phi 0/0? (y/n)").upper()
+    if start == "Y":
+        pass
+    else:
+        sys.exit("To start, centre the pin at kappa/phi 0/0 then restart the script")
+    xZ, yZ, zZ = getZero()
+    while True:
+        RBVS = getReadbacks(xZ, yZ, zZ)
+        print(RBVS)
+        with open(f'rbvs_{now.strftime("%d%m%Y")}.txt', 'a') as f:
+            f.write(str(RBVS))
+            f.write('\n')
+        next = input("Press Enter when centered to record new position, or press Q to finish: ").upper()
+        if next == "Q":
+            sys.exit(0)
+        else:
+            pass
