2fbe055fc79981589d995daa2f3715101bda140d
co2e14 <christian.orr@diamond.ac.uk>
2024-06-03 11:41:05 +0100

    should be omega independent

--- collectData.py
+++ collectData.py
@@ -8,11 +8,11 @@ def getReadbacks():
     x = caget('BL23I-MO-GONIO-01:X')
     y = caget('BL23I-MO-GONIO-01:Y')
     z = caget('BL23I-MO-GONIO-01:Z')
-    omega = caget('BL23I-MO-GONIO-01:OMEGA')
+    #omega = caget('BL23I-MO-GONIO-01:OMEGA')
     kappa = caget('BL23I-MO-GONIO-01:KAPPA')
     phi = caget('BL23I-MO-GONIO-01:PHI')
     
-    vals = [np.round(float(x), 4), np.round(float(y), 4), np.round(float(z), 4), np.round(float(omega), 1), np.round(float(kappa), 1), np.round(float(phi), 1)]
+    vals = [np.round(float(x), 4), np.round(float(y), 4), np.round(float(z), 4), np.round(float(kappa), 1), np.round(float(phi), 1)]
 
     return vals
 
